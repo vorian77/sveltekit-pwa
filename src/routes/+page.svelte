a7d749932c1cc84f8943839ec50fe7fb88652b46
Phyllip Hall <phall776@gmail.com>
2023-04-06 08:08:19 -0400

    test: production ionic

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,25 +5,6 @@
 } -->
 <script>
 	import { Geolocation } from '@capacitor/geolocation';
-
-	let loc = null;
-	async function getCurrentPosition() {
-		const res = await Geolocation.getCurrentPosition();
-		loc = res;
-	}
-</script>
-
-<!-- <h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
-</style> -->
-<!-- <script>
-	import { Page, Navbar, Block, Button, List, ListItem, BlockTitle } from 'konsta/svelte';
-
-	import { Geolocation } from '@capacitor/geolocation';
 	import { Camera, CameraResultType } from '@capacitor/camera';
 
 	let loc = null;
@@ -45,100 +26,27 @@
 		// passed to the Filesystem API to read the raw data of the image,
 		// if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
 		picture = image.webPath;
-		alert(picture);
-
-		// Can be set to the src of an image now
-		//imageElement.src = imageUrl;
 	};
-</script> -->
-
-<!-- <script lang="ts">
-	import { Page, Navbar, Block, Button, List, ListItem, BlockTitle } from 'konsta/svelte';
 </script>
 
-<Page>
-	<Navbar title="My App" />
-
-	<Block strong>
-		<p>Here is your SvelteKit & Konsta UI app. Let's see what we have here.</p>
-	</Block>
-	<BlockTitle>Navigation</BlockTitle>
-	<List>
-		<ListItem href="/about/" title="About" />
-		<ListItem href="/form/" title="Form" />
-	</List>
-
-	<Block strong class="flex space-x-4">
-		<Button>Button 1</Button>
-		<Button>Button 2</Button>
-	</Block>
-</Page> -->
-
-<!-- <Page>
-	<Navbar title="My App" />
-
-	<Block strong>
-		<p>Here is your SvelteKit & Konsta UI app. Let's see what we have here.</p>
-	</Block>
-	<BlockTitle>Navigation</BlockTitle>
-	<List>
-		<ListItem href="/about/" title="About" />
-		<ListItem href="/form/" title="Form" />
-	</List>
-
-	<Block strong class="flex space-x-4">
-		<Button>Button 1</Button>
-		<Button>Button 2</Button>
-	</Block>
-
-	<Block strong>
-		<div>
-			<h1>Geolocation</h1>
-			<p>Your location is:</p>
-			<p>Latitude: {loc?.coords.latitude}</p>
-			<p>Longitude: {loc?.coords.longitude}</p>
-
-			<button on:click={getCurrentPosition}> Get Current Location </button>
-		</div>
-	</Block>
-
-	<Block strong>
-		<div>
-			<Button onClick={takePicture}>Take picture1</Button>
-			<img class="fit-picture" src={picture} alt="pic from camera" />
-		</div>
-	</Block>
-</Page> -->
-
-<ion-header translucent="true">
-	<ion-toolbar>
-		<ion-buttons slot="start">
-			<ion-menu-button />
-		</ion-buttons>
-		<ion-buttons slot="end"> A button </ion-buttons>
-		<ion-title>Card</ion-title>
-	</ion-toolbar>
-</ion-header>
-
 <ion-content fullscreen>
-	<ion-button>Default1</ion-button>
-	<ion-button disabled="true">Disabled</ion-button>
+	<ion-card>
+		<ion-title size="large">Ionic - Geolocation</ion-title>
+		<p>Your location is:</p>
+		<p>Latitude: {loc?.coords.latitude}</p>
+		<p>Longitude: {loc?.coords.longitude}</p>
+		<ion-button on:click={getCurrentPosition}> Get Current Location </ion-button>
+	</ion-card>
 
 	<ion-card>
-		<ion-card-header>
-			<ion-card-subtitle>Card Subtitle</ion-card-subtitle>
-			<ion-card-title>Card Title</ion-card-title>
-		</ion-card-header>
-
-		<ion-card-content>
-			Keep close to Nature's heart... and break clear away, once in awhile, and climb a mountain or
-			spend a week in the woods. Wash your spirit clean.
-		</ion-card-content>
+		<ion-title size="large">Ionic - Camera</ion-title>
+		<img class="fit-picture" src={picture} alt="pic from camera" />
+		<ion-button on:click={takePicture}>Take Picture</ion-button>
 	</ion-card>
-	<h1>Geolocation</h1>
-	<p>Your location is:</p>
-	<p>Latitude: {loc?.coords.latitude}</p>
-	<p>Longitude: {loc?.coords.longitude}</p>
-
-	<button on:click={getCurrentPosition}> Get Current Location </button>
 </ion-content>
+
+<style>
+	ion-title {
+		--color: #21a461;
+	}
+</style>
